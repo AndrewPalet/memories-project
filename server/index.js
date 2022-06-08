b2a55756646904a0401a8566d87e20b336d50866
ectaw6d <Andrew.Palet@McKesson.com>
2022-06-08 15:31:40 -0500

    configured node server using js and connected to mongoDB

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true}));
+app.use(express.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://andrewpalet:andrewpalet123@cluster0.2xct6.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
