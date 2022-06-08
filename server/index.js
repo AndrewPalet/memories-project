42102183905bf8418e6403b55d7a5d44ac01d36d
ectaw6d <Andrew.Palet@McKesson.com>
2022-06-08 15:43:36 -0500

    taking out mongoDB password

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://andrewpalet:andrewpalet123@cluster0.2xct6.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://andrewpalet:<password>@cluster0.2xct6.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL)
