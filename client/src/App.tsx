606ec53745826b186e51f183384652dcc04e7feb
ectaw6d <Andrew.Palet@McKesson.com>
2022-06-21 23:12:54 -0500

    creating forms and post components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
+import Form from './components/Form/Form';
+import PostList from './components/PostList/PostList';
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+       <Container maxWidth="lg"> 
+           <AppBar position="static" color="inherit">
+               <Typography align="center" variant="h2">Memories</Typography>
+               <img src={memories} alt="memories" height="200" />
+           </AppBar>
+
+           <Grow in>
+               <Container>
+                   <Grid container justify="space-between" alignItems="center" spacing={3}>
+                       <Grid item xs={12} sm={7} >
+                           <Form />
+                       </Grid>
+                       <Grid item xs={12} sm={4} >
+                           <PostList />
+                       </Grid>
+                   </Grid>
+               </Container>
+           </Grow>
+       </Container>
     );
 }
 
---
+++ client/src/components/PostList/PostList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const PostList = () => {
+  return (
+    <>
+      <h1>POST</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default PostList;
